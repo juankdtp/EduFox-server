08256f6cadfc94189780b79b9d61a4000409fa4f
hrndbrs <hrndbrs@gmail.com>
2023-11-22 12:08:58 +0700

    fixed: feedback controller
    -add access token to response after creating new feedback row

--- controllers/feedbackController.js
+++ controllers/feedbackController.js
@@ -1,25 +1,31 @@
-const { Feedback, Course, User } = require("../models/index");
+const { Feedback, Course, User, sequelize } = require("../models/index");
+const { tokenCreate } = require("../helpers/jwt");
 
 class FeedbackController {
   static async addFeedback(req, res, next) {
     const { userId } = req.user;
     const { courseId } = req.params;
     const { rating, comment } = req.body;
+
+    const transaction = await sequelize.transaction()
+
     try {
       const checkFeedback = await Feedback.findOne({
         where: {
           CourseId: courseId,
           UserId: userId,
         },
+        transaction
       });
 
       const feedback = await Feedback.findAndCountAll({
         where: {
           CourseId: courseId,
         },
+        transaction
       });
 
-      const user = await User.findByPk(userId);
+      const user = await User.findByPk(userId, { transaction });
       // console.log(user, 23);
       const userPoint = user.point;
       // console.log(userPoint, 25);
@@ -62,7 +68,7 @@ class FeedbackController {
         // console.log(meanRating, 70);
         // const courseCheck = await Course.findByPk(courseId);
         // console.log(courseCheck, 72);
-        const updateCourse = await Course.update(
+        await Course.update(
           {
             rating: meanRating,
           },
@@ -70,12 +76,26 @@ class FeedbackController {
             where: {
               id: courseId,
             },
+            transaction
           }
         );
         // console.log(updateCourse, 82);
         // console.log(courseId, 83);
 
-        const updateFeedback = await Feedback.update(
+        // const updateFeedback = await Feedback.update(
+        //   {
+        //     rating,
+        //     comment,
+        //   },
+        //   {
+        //     where: {
+        //       CourseId: courseId,
+        //       UserId: userId,
+        //     },
+        //     transaction
+        //   }
+        // );
+        await Feedback.update(
           {
             rating,
             comment,
@@ -85,12 +105,16 @@ class FeedbackController {
               CourseId: courseId,
               UserId: userId,
             },
+            transaction
           }
         );
 
+        transaction.commit()
+
         res.status(200).json({
           statusCode: 200,
-          data: updateFeedback,
+
+          // data: updateFeedback,
         });
       } else {
         // console.log(totalRating, 98);
@@ -99,7 +123,7 @@ class FeedbackController {
         meanRating = totalRating / (feedback.count + 1);
         // console.log(meanRating, 102);
 
-        const updateCourse = await Course.update(
+        await Course.update(
           {
             rating: meanRating,
           },
@@ -107,10 +131,11 @@ class FeedbackController {
             where: {
               id: courseId,
             },
+            transaction
           }
         );
 
-        const updateUser = await User.update(
+        const [_, [updateUser]] = await User.update(
           {
             point: userPoint + 5,
           },
@@ -118,22 +143,45 @@ class FeedbackController {
             where: {
               id: userId,
             },
+            returning: true,
+            transaction
           }
         );
 
-        const createFeedback = await Feedback.create({
+        const payload = {
+          userId: updateUser.id,
+          userUsername: updateUser.username,
+          userEmail: updateUser.email,
+          userPremium: updateUser.isPremium,
+          userPoint: updateUser.point,
+          userProfilePict: updateUser.profilePicture,
+        };
+
+        const access_token = tokenCreate(payload)
+
+        // const createFeedback = await Feedback.create({
+        //   rating,
+        //   comment,
+        //   CourseId: courseId,
+        //   UserId: userId,
+        // });
+        await Feedback.create({
           rating,
           comment,
           CourseId: courseId,
           UserId: userId,
-        });
+        }, { transaction });
+
+        transaction.commit()
 
         res.status(200).json({
           statusCode: 200,
-          data: createFeedback,
+          access_token
+          // data: createFeedback,
         });
       }
     } catch (err) {
+      await transaction.rollback()
       console.log(err);
       next(err);
     }
